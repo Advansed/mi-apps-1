58d98a31b8f6e521d866598937cc7adf80a623fa
Advansed <atlasov.n.r@gmail.com>
2025-08-28 13:22:30 +0900

    first

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mi-app-1',
+  webDir: 'dist'
+};
+
+export default config;
